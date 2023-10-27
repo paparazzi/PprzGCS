675a86918d96f68b17d646ac74ad3c0cf8762550
alfredoFBW <alfredgo@ucm.es>
2023-10-27 09:04:13 +0200

    Changed default number of segments to 1, same as in paparazzi

--- src/widgets/map/gvf_trajectories/gvf_traj_bezier.h
+++ src/widgets/map/gvf_trajectories/gvf_traj_bezier.h
@@ -5,29 +5,29 @@
 
 class GVF_traj_bezier : public GVF_trajectory
 {
-    Q_OBJECT
+  Q_OBJECT
 public:
-    explicit GVF_traj_bezier(QString id, QList<float> param, QList<float> _phi,
-    			      float wb, QVector<int> *gvf_settings);
+  explicit GVF_traj_bezier(QString id, QList<float> param, QList<float> _phi,
+                           float wb, QVector<int> *gvf_settings);
 
 protected:
-    virtual void genTraj() override;
-    virtual void genVField() override;
+  virtual void genTraj() override;
+  virtual void genVField() override;
 
 private:
-    void set_param(QList<float> param, QList<float> _phi, float wb); // GVF PARAMETRIC
-    QPointF traj_point(float t);
-    QPointF traj_point_deriv(float t);
-    
-    // Maximum data size from gvf_parametric is 16 elements.
-    float xx[16];
-    float yy[16];
-    int n_seg;
-    float w;
-    float kx;
-    float ky;
-    float beta;
-    QPointF phi;
+  void set_param(QList<float> param, QList<float> _phi, float wb); // GVF PARAMETRIC
+  QPointF traj_point(float t);
+  QPointF traj_point_deriv(float t);
+
+  // Maximum data size from gvf_parametric is 16 elements.
+  float xx[16];
+  float yy[16];
+  int n_seg;
+  float w;
+  float kx;
+  float ky;
+  float beta;
+  QPointF phi;
 };
 
 #endif // GVF_TRAJ_bezier_H
