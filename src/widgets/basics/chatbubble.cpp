4c3759cdca89250ce98e497c2a25fe1fcff85308
Freek van Tienen <freek.v.tienen@gmail.com>
2024-05-16 15:17:29 +0200

    Update chatbubble layout and combine messages

--- src/widgets/basics/chatbubble.cpp
+++ src/widgets/basics/chatbubble.cpp
@@ -1,12 +1,13 @@
 #include "chatbubble.h"
 #include "ui_chatbubble.h"
-#include <QTime>
 #include "AircraftManager.h"
 #include <QDebug>
 
 ChatBubble::ChatBubble(QString source, QString dest, QString msg, bool sent, QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::ChatBubble)
+    ui(new Ui::ChatBubble),
+    source(source),
+    dest(dest)
 {
     ui->setupUi(this);
 
@@ -22,10 +23,18 @@ ChatBubble::ChatBubble(QString source, QString dest, QString msg, bool sent, QWi
         source_name += QString::fromUtf8(" \u2794 ") + dest;
     }
 
-    auto date = QTime::currentTime().toString();
-    ui->name_label->setText("<b>"+source_name+"</b> "+date+" :");
+    // Add option for bold text
+    msg = msg.toHtmlEscaped();
+    QRegularExpression re(QStringLiteral("\\*([^*]+)\\*"));
+    msg.replace(re, "<b>\\1</b>");
+
+    chat_time = QTime::currentTime();
+    auto date = chat_time.toString();
+    ui->time_label->setText(date+" ");
+    ui->name_label->setText("<b>"+source_name+"</b>");
     ui->name_label->setStyleSheet("color:" + color.name());
     ui->message_label->setText(msg);
+    ui->message_label->setTextFormat(Qt::RichText);
 
     if(sent) {
         ui->name_label->setAlignment(Qt::AlignLeft);
@@ -37,7 +46,7 @@ ChatBubble::ChatBubble(QString source, QString dest, QString msg, bool sent, QWi
 
     // delay set max width to wait for the widget to be shown.
     QTimer::singleShot(20, this, [=](){
-            auto width = size().width() * 0.7;
+            auto width = size().width() * 0.95;
             ui->frame->setMaximumWidth(width);
     });
 }
@@ -47,11 +56,22 @@ ChatBubble::~ChatBubble()
     delete ui;
 }
 
+void ChatBubble::addText(QString text) {
+    // Add option for bold text
+    text = text.toHtmlEscaped();
+    QRegularExpression re(QStringLiteral("\\*([^*]+)\\*"));
+    text.replace(re, "<b>\\1</b>");
+
+    chat_time = QTime::currentTime();
+    ui->message_label->setText(ui->message_label->text() + "<br>" + text);
+    ui->message_label->setTextFormat(Qt::RichText);
+}
+
 bool ChatBubble::eventFilter(QObject *obj, QEvent *ev) {
     (void)obj;
     (void)ev;
     if(ev->type() == QEvent::Resize) {
-        auto width = size().width() * 0.7;
+        auto width = size().width() * 0.95;
         ui->frame->setMaximumWidth(width);
     }
 
--- src/widgets/basics/chatbubble.h
+++ src/widgets/basics/chatbubble.h
@@ -2,6 +2,7 @@
 #define CHATBUBBLE_H
 
 #include <QWidget>
+#include <QTime>
 
 namespace Ui {
 class ChatBubble;
@@ -12,14 +13,23 @@ class ChatBubble : public QWidget
     Q_OBJECT
 
 public:
-    explicit ChatBubble(QString sender, QString dest, QString msg, bool sent=false, QWidget *parent = nullptr);
+    explicit ChatBubble(QString source, QString dest, QString msg, bool sent=false, QWidget *parent = nullptr);
     ~ChatBubble();
+
+    QString getSource() {return source;}
+    QString getDest() {return dest;}
+    QTime getTime() {return chat_time;}
+    void addText(QString text);
 
 protected:
     bool eventFilter(QObject *obj, QEvent *ev) override;
 
 private:
     Ui::ChatBubble *ui;
+
+    QString source;
+    QString dest;
+    QTime chat_time;
 };
 
 #endif // CHATBUBBLE_H
